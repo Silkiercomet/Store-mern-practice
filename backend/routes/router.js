1ce39a05d98bc21b06075d0f5eeade64c7d49fad
luis Colina <colina.1990lz@gmail.com>
2023-03-31 17:00:25 -0400

    documentation a step by step guide added, also comments on the code

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,12 +1,19 @@
 const express = require("express");
+
+// 1- creamos uns instacia de express.Router()
 const recordRoutes = express.Router();
+
+// 2- importamos nuestros controladores, la logica implementada en su respectiva ruta
 const {
   getProducts,
   getProductById,
 } = require("../controllers/controls");
 
+// 3- llamamos al objeto router, definimos nuestra ruta y el metodo (get, post, update, delete) que utilizaremos   
 recordRoutes.route("/").get(getProducts);
 
+// 4- definimos un parametro es nuestra ruta ":" en este caso "id" que sera utilizado para tomar un valor especifco de la db
 recordRoutes.route("/product/:id").get(getProductById);
 
+// 5- exportamos nuestra instancia del objeto Router
 module.exports = recordRoutes;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,41 @@
+// express : fremework de node.js
 const express = require("express");
+
+// mongoose : interactua con mongodb
 const mongoose = require("mongoose");
+
+// cors permite que la app interactue con diferentes webs, EJ: localhost | atlas.mongodb.com
 const cors = require("cors");
+
+// body-parser : formatea los body request al recibirlos
 const bodyParser = require("body-parser");
+
+// permite crear variables privadas .env
 const dotenv = require("dotenv");
+
+// 1- se crea una instancia de express en la constante app
 const app = express();
+// 2- se importa el router que contiene nuestras rutas
 const recordRoutes = require("./routes/router");
 
+// 3- se inicializa nuestros paquetes
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config({ path: "./config.env" });
 
+// 4- conectamos nuestra app con su respectiva base de datos
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+// 5- asignamos un puerto local para nuestra app 
 const port = process.env.PORT || 5000;
 
+// 6- formateamos nuestra ruta
 app.use("/api", recordRoutes);
 
+// 7- empezamos a escuchar los request a nuestras rutas 
 app.listen(port, (req, res) => {
   console.log(`Server running on port ${port}`);
 });
